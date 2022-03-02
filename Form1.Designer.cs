7773188f9f1d232d476081d44594a66270e554dc
Alexander Anderson <ksdrago@yandex.ru>
2022-03-02 12:12:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace domain_setings_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.input_domain = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.shutdown = new System.Windows.Forms.Button();
+            this.PowerShell = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button_exit_domain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // input_domain
+            // 
+            this.input_domain.Location = new System.Drawing.Point(363, 25);
+            this.input_domain.Name = "input_domain";
+            this.input_domain.Size = new System.Drawing.Size(136, 23);
+            this.input_domain.TabIndex = 1;
+            this.input_domain.Text = "Ввести в домен";
+            this.input_domain.UseVisualStyleBackColor = true;
+            this.input_domain.Click += new System.EventHandler(this.input_domain_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя компьютера";
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(363, 54);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(136, 23);
+            this.reset.TabIndex = 3;
+            this.reset.Text = "Перезагрузка";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // shutdown
+            // 
+            this.shutdown.Location = new System.Drawing.Point(363, 83);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(136, 23);
+            this.shutdown.TabIndex = 4;
+            this.shutdown.Text = "Выключение";
+            this.shutdown.UseVisualStyleBackColor = true;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // PowerShell
+            // 
+            this.PowerShell.Location = new System.Drawing.Point(363, 113);
+            this.PowerShell.Name = "PowerShell";
+            this.PowerShell.Size = new System.Drawing.Size(136, 23);
+            this.PowerShell.TabIndex = 5;
+            this.PowerShell.Text = "Проверка на доверие";
+            this.PowerShell.UseVisualStyleBackColor = true;
+            this.PowerShell.Click += new System.EventHandler(this.PowerShell_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(16, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Наличие в домене";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(16, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(74, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Доверие ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button_exit_domain
+            // 
+            this.button_exit_domain.Location = new System.Drawing.Point(363, 142);
+            this.button_exit_domain.Name = "button_exit_domain";
+            this.button_exit_domain.Size = new System.Drawing.Size(136, 23);
+            this.button_exit_domain.TabIndex = 9;
+            this.button_exit_domain.Text = "Выйти из домена";
+            this.button_exit_domain.UseVisualStyleBackColor = true;
+            this.button_exit_domain.Click += new System.EventHandler(this.button_exit_domain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 176);
+            this.Controls.Add(this.button_exit_domain);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.PowerShell);
+            this.Controls.Add(this.shutdown);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_domain);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(549, 215);
+            this.MinimumSize = new System.Drawing.Size(549, 215);
+            this.Name = "Form1";
+            this.Text = "Domain master ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button input_domain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button shutdown;
+        private System.Windows.Forms.Button PowerShell;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button_exit_domain;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace domain_setings_winforms
+{
+    public partial class Form1 : Form
+    {
+        string name_machine_in_domain = "";
+        string name_machine = "";
+        bool domain_status;
+        bool trust_domain_bool;
+        public Form1()
+        {
+            InitializeComponent();
+            domain_name();
+            PartOfDomain();
+            this.Text = "Domain master " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            textBox1.Multiline = false;
+            if (Properties.Settings.Default.name != null && Properties.Settings.Default.name !="")
+            {
+                textBox1.Text = Properties.Settings.Default.name;
+                name_machine = Properties.Settings.Default.name;
+            }
+        }
+
+        private void input_domain_Click(object sender, EventArgs e)
+        {
+            name_machine = textBox1.Text;
+            changes_name(name_machine);
+        }
+        private void changes_name(string newName)
+        {
+            if (newName != null && newName !="")
+            {
+                RegistryKey key = Registry.LocalMachine;
+                string activeComputerName = "SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName";
+                RegistryKey activeCmpName = key.CreateSubKey(activeComputerName);
+                activeCmpName.SetValue("ComputerName", newName);
+                activeCmpName.Close();
+                string computerName = "SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ComputerName";
+                RegistryKey cmpName = key.CreateSubKey(computerName);
+                cmpName.SetValue("ComputerName", newName);
+                cmpName.Close();
+                string _hostName = "SYSTEM\\CurrentControlSet\\services\\Tcpip\\Parameters\\";
+                RegistryKey hostName = key.CreateSubKey(_hostName);
+                hostName.SetValue("Hostname", newName);
+                hostName.SetValue("NV Hostname", newName);
+                hostName.Close();
+                new_domain(newName);
+            }
+            else
+            {
+                MessageBox.Show("Не задано имя компьютера!");
+            }
+        }
+        private void new_domain(string newName)
+        {
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                StandardErrorEncoding = Encoding.GetEncoding(866),
+                StandardOutputEncoding = Encoding.GetEncoding(866),
+                Arguments = "/c netdom join " + newName + " /domain:radio1.aqua.sci-nnov.ru /userd:1 /passwordd:362",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            });;
+            string result_Output = process.StandardOutput.ReadToEnd().Replace("Active code page: 65001", "");
+            string result_Error = process.StandardError.ReadToEnd();
+            if (!String.IsNullOrWhiteSpace(result_Output) && !String.IsNullOrEmpty(result_Output))
+            {
+                MessageBox.Show(result_Output, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+            {
+                MessageBox.Show(result_Error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            PartOfDomain();
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = "/c shutdown /r /t 0",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.name = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                Properties.Settings.Default.name = textBox1.Text;
+                Properties.Settings.Default.Save();
+                input_domain_Click(sender, e);
+            }
+        }
+        private void shutdown_Click(object sender, EventArgs e)
+        {
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = "/c shutdown /s /t 0",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        private void PowerShell_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    Process process = Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "powershell",
+                        Arguments = "/c Test-ComputerSecureChannel",
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+
+                        StandardErrorEncoding = Encoding.GetEncoding(866),
+                        StandardOutputEncoding = Encoding.GetEncoding(866),
+                    }); ;
+                    string result_Outrut = process.StandardOutput.ReadToEnd();
+                    string result_Error = process.StandardError.ReadToEnd();
+                    if (!String.IsNullOrWhiteSpace(result_Outrut) && !String.IsNullOrEmpty(result_Outrut))
+                    {
+                        if (result_Outrut.Contains("True"))
+                        {
+                            MessageBox.Show("Доверительные отношения установленны", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            trust_domain_bool = true;
+                        }
+                        else if (result_Outrut.Contains("False"))
+                        {
+                            MessageBox.Show("Доверительные отношения ОТСУТСТВУЮТ", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            trust_domain_bool = false;
+                        }
+                        checkBox2.Checked = trust_domain_bool;
+                    }
+                    if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+                    {
+                        MessageBox.Show(result_Error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+        private void PartOfDomain()
+        {
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                StandardErrorEncoding = Encoding.GetEncoding(866),
+                StandardOutputEncoding = Encoding.GetEncoding(866),
+                Arguments = "/c wmic ComputerSystem get PartOfDomain",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            }); ;
+            string result_Output = process.StandardOutput.ReadToEnd().Replace("PartOfDomain", "").Trim();
+            string result_Error = process.StandardError.ReadToEnd();
+            if (!String.IsNullOrWhiteSpace(result_Output) && !String.IsNullOrEmpty(result_Output))
+            {
+                if (result_Output=="TRUE")
+                {
+                    domain_status = true;
+                }
+                else if (result_Output=="FALSE")
+                {
+                    domain_status = false;
+                }
+                checkBox1.Checked = domain_status;
+                if (domain_status)
+                {
+                    input_domain.Enabled = false;
+                    PowerShell.Enabled = domain_status;
+                    trust_domain();
+                }
+                else
+                {
+                    input_domain.Enabled=true;
+                    PowerShell.Enabled = domain_status;
+                    checkBox2.Checked = domain_status;
+                }
+            }
+            if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+            {
+                MessageBox.Show(result_Error, "Ошибка при получении информации о состоянии домена", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void domain_name()
+        {
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                StandardErrorEncoding = Encoding.GetEncoding(866),
+                StandardOutputEncoding = Encoding.GetEncoding(866),
+                Arguments = "/c wmic ComputerSystem get Name",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            }); ;
+            string result_Output = process.StandardOutput.ReadToEnd().Replace("Name", "").Trim();
+            string result_Error = process.StandardError.ReadToEnd();
+            if (!String.IsNullOrWhiteSpace(result_Output) && !String.IsNullOrEmpty(result_Output))
+            {
+                name_machine_in_domain=result_Output;
+                label1.Text = "Имя компьютера: " + result_Output;
+            }
+            if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+            {
+                MessageBox.Show(result_Error, "Ошибка при получении имени компьютера", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void trust_domain()
+        {
+            try
+            {
+                Process process = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "powershell",
+                    Arguments = "/c Test-ComputerSecureChannel",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                }); ;
+                string result_Outrut = process.StandardOutput.ReadToEnd();
+                string result_Error = process.StandardError.ReadToEnd();
+                if (!String.IsNullOrWhiteSpace(result_Outrut) && !String.IsNullOrEmpty(result_Outrut))
+                {
+                    if (result_Outrut.Contains("True"))
+                    {
+                        trust_domain_bool = true;
+                    }
+                    else if (result_Outrut.Contains("False"))
+                    {
+                        trust_domain_bool = false;
+                    }
+                    checkBox2.Checked = trust_domain_bool;
+                }
+                if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+                {
+                    MessageBox.Show(result_Error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_exit_domain_Click(object sender, EventArgs e)
+        {
+            domain_name();
+            Process process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                StandardErrorEncoding = Encoding.GetEncoding(866),
+                StandardOutputEncoding = Encoding.GetEncoding(866),
+                Arguments = "/c netdom remove " + name_machine_in_domain + " /Domain:radio1.aqua.sci-nnov.ru /UserD:1 /PasswordD:362 /Force",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            }); ;
+            string result_Output = process.StandardOutput.ReadToEnd().Replace("Active code page: 65001", "");
+            string result_Error = process.StandardError.ReadToEnd();
+            if (!String.IsNullOrWhiteSpace(result_Output) && !String.IsNullOrEmpty(result_Output))
+            {
+                MessageBox.Show(result_Output, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!String.IsNullOrWhiteSpace(result_Error) && !String.IsNullOrEmpty(result_Error))
+            {
+                MessageBox.Show(result_Error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            PartOfDomain();
+        }
+    }
+}
